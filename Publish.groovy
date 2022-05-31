ec1e60006fc6c59aaa495af14c63e3ff48cab564
Sven Jost <sven@wire.com>
2022-05-31 15:20:02 +0200

    Add Publish pipeline

---
+++ Publish.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent 'built-in'
+    stages {
+        stage('Get artifacts') {
+            steps {
+                copyArtifacts filter: 'webrtc_*.zip', fingerprintArtifacts: true, projectName: 'webrtc', selector: buildParameter('BUILD_SELECTOR')
+            }
+        }
+        stage('Publish') {
+            steps {
+                withCredentials([string(credentialsId: 'github-repo-access', variable: 'token')]) {
+                    sh '''
+                    export GITHUB_TOKEN=$token
+                    . scripts/version.sh
+                    python3 scripts/push2github.py
+                    '''
+                }
+            }
+        }
+    }
+}
