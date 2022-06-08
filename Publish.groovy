19a5ec7f19b0258140ef1e243765bf563d8540f3
Sven Jost <sven@wire.com>
2022-06-08 17:13:14 +0200

    Adjust environment variable for push script

--- Publish.groovy
+++ Publish.groovy
@@ -2,11 +2,11 @@ pipeline {
     agent {
         label 'built-in'
     }
-    parameters { string(name: 'PARENT_BUILD_NUMBER', defaultValue: '', description: 'Build number of the webrtc-prebuild-new to be published') }
+    parameters { string(name: 'PARENT_BUILDNUMBER', defaultValue: '', description: 'Build number of the webrtc-prebuild-new to be published') }
     stages {
         stage('Get artifacts') {
             steps {
-                copyArtifacts filter: 'webrtc*.zip', fingerprintArtifacts: true, projectName: 'webrtc-prebuild-new', selector: specific("${PARENT_BUILD_NUMBER}")
+                copyArtifacts filter: 'webrtc*.zip', fingerprintArtifacts: true, projectName: 'webrtc-prebuild-new', selector: specific("${PARENT_BUILDNUMBER}")
             }
         }
         stage('Publish') {
@@ -15,7 +15,7 @@ pipeline {
                     sh '''
                     export GITHUB_TOKEN=$token
                     . scripts/version.sh
-                    python3 scripts/push2github.py
+                    BUILDNUMBER=$PARENT_BUILDNUMBER python3 scripts/push2github.py
                     '''
                 }
             }
