4f3221b7072b5decfdb1d4be4ae567177272f9ad
Sven Jost <sven@wire.com>
2022-06-08 17:16:01 +0200

    Fix typo in env variable

--- Publish.groovy
+++ Publish.groovy
@@ -2,11 +2,11 @@ pipeline {
     agent {
         label 'built-in'
     }
-    parameters { string(name: 'PARENT_BUILDNUMBER', defaultValue: '', description: 'Build number of the webrtc-prebuild-new to be published') }
+    parameters { string(name: 'PARENT_BUILD_NUMBER', defaultValue: '', description: 'Build number of the webrtc-prebuild-new to be published') }
     stages {
         stage('Get artifacts') {
             steps {
-                copyArtifacts filter: 'webrtc*.zip', fingerprintArtifacts: true, projectName: 'webrtc-prebuild-new', selector: specific("${PARENT_BUILDNUMBER}")
+                copyArtifacts filter: 'webrtc*.zip', fingerprintArtifacts: true, projectName: 'webrtc-prebuild-new', selector: specific("${PARENT_BUILD_NUMBER}")
             }
         }
         stage('Publish') {
@@ -15,7 +15,7 @@ pipeline {
                     sh '''
                     export GITHUB_TOKEN=$token
                     . scripts/version.sh
-                    BUILDNUMBER=$PARENT_BUILDNUMBER python3 scripts/push2github.py
+                    BUILD_NUMBER=$PARENT_BUILD_NUMBER python3 scripts/push2github.py
                     '''
                 }
             }
