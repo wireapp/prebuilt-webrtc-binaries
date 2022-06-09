79df7c83b9664642f7b98513ea5f60be57d88a26
Sven Jost <sven@wire.com>
2022-06-09 15:35:00 +0200

    Add clean step to not upload everything again

--- Publish.groovy
+++ Publish.groovy
@@ -4,6 +4,11 @@ pipeline {
     }
     parameters { string(name: 'PARENT_BUILD_NUMBER', defaultValue: '', description: 'Build number of the webrtc-prebuild-new to be published') }
     stages {
+        stage('Clean') {
+            steps {
+                sh 'rm -rf webrtc*.zip'
+            }
+        }
         stage('Get artifacts') {
             steps {
                 copyArtifacts filter: 'webrtc*.zip', fingerprintArtifacts: true, projectName: 'webrtc-prebuild-new', selector: specific("${PARENT_BUILD_NUMBER}")
