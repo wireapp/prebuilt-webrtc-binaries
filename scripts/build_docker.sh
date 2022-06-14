2de58e19b7d8f6ed30698e6a5f22224f66ac2e19
Sven Jost <sven@wire.com>
2022-06-14 10:46:42 +0200

    Use one dependencies script for docker and vagrant

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build . -t webrtc
-docker run -v $PWD:/out -t webrtc /bin/bash -c "scripts/build_linux.sh; scripts/package.sh; cp *.zip /out"
+docker run -v $PWD:/out -t webrtc /bin/bash -c "scripts/build_linux.sh; cp *.zip /out"
