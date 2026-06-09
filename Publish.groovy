6b9d1778fc2dc65382985b4eb7ce7c2c9680529d
Dusan Stevanovic <dusan@wire.com>
2026-06-09 23:28:12 +0200

    Install PyGithub attempt 2

--- Publish.groovy
+++ Publish.groovy
@@ -19,7 +19,7 @@ pipeline {
                 withCredentials([string(credentialsId: 'github-repo-access', variable: 'token')]) {
                     sh(
 		        script: """
-			    python3 -m pip install PyGithub
+			    pip3 install PyGithub
 			    scripts/version.sh
                             GITHUB_TOKEN=$token
 			    BUILD_NUMBER=$PARENT_BUILD_NUMBER 			    
