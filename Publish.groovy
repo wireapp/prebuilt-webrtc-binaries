4d9798cca0b8c3b541650f6d684dd2ff9fc5b023
Dusan Stevanovic <dusan@wire.com>
2026-06-11 10:28:28 +0200

    Use sh Publish script

--- Publish.groovy
+++ Publish.groovy
@@ -17,14 +17,11 @@ pipeline {
         stage('Publish') {
             steps {
                 withCredentials([string(credentialsId: 'github-repo-access', variable: 'token')]) {
-                    sh(
-		        script: """
-			    scripts/version.sh
-                            GITHUB_TOKEN=$token
-			    BUILD_NUMBER=$PARENT_BUILD_NUMBER 			    
-                            python3 ./scripts/push2github.py
-                        """
-		    )
+                    sh '''
+                    export GITHUB_TOKEN=$token
+                    . scripts/version.sh
+                    BUILD_NUMBER=$PARENT_BUILD_NUMBER python3 scripts/push2github.py
+                    '''
                 }
             }
         }
